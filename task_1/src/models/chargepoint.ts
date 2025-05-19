17d4528201546d2ca0a2400deb668b8fddf1c687
proyev <prots.yevhen@gmail.com>
2025-05-20 00:11:03 +0200

    create chargepoint class

---
+++ task_1/src/models/chargepoint.ts
@@ -0,0 +1,65 @@
+import { v4 as uuidv4 } from "uuid";
+import { Chargepoint as IChargepoint } from "./types";
+import {
+  DEFAULT_CHARGEPOINT_POWER,
+  DEFAULT_EV_CONSUMPTION,
+  TIME_CONSTANTS,
+} from "../config/constants";
+
+export class Chargepoint implements IChargepoint {
+  private _id: string;
+  private _isOccupied: boolean = false;
+  private _remainingChargingTime: number = 0; // in ticks
+  private _chargePower: number;
+  private _totalCharges: number = 0;
+  private _totalEnergyConsumption: number = 0;
+
+  constructor(id = uuidv4(), chargePower = DEFAULT_CHARGEPOINT_POWER) {
+    this._id = id;
+    this._chargePower = chargePower;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get isOccupied(): boolean {
+    return this._isOccupied;
+  }
+
+  get chargePower(): number {
+    return this._chargePower;
+  }
+
+  get remainingChargingTime(): number {
+    return this._remainingChargingTime;
+  }
+
+  get totalCharges(): number {
+    return this._totalCharges;
+  }
+
+  get totalEnergyConsumption(): number {
+    return this._totalEnergyConsumption;
+  }
+
+  startCharging(demandKm: number): void {
+    if (this._isOccupied || demandKm <= 0) return; // already occupied or invalid demand
+
+    const energyDemand = (demandKm / 100) * DEFAULT_EV_CONSUMPTION; // in kWh
+    const chargingDuration = energyDemand / this._chargePower; // in hours
+    this._remainingChargingTime = Math.ceil(
+      chargingDuration * TIME_CONSTANTS.TICKS_PER_HOUR,
+    ); // convert to time demand in ticks
+
+    this._isOccupied = true;
+    this._totalCharges++;
+  }
+
+  reset(): void {
+    this._isOccupied = false;
+    this._remainingChargingTime = 0;
+    this._totalEnergyConsumption = 0;
+    this._totalCharges = 0;
+  }
+}
--- task_1/src/models/types.ts
+++ task_1/src/models/types.ts
@@ -2,3 +2,14 @@ export type ChargingDemand = {
   probability: number;
   demand: number; // in km
 };
+
+export interface Chargepoint {
+  id: string;
+  isOccupied: boolean;
+  chargePower: number; // in kW
+  totalCharges: number;
+  totalEnergyConsumption: number; // in kWh
+
+  startCharging(demandKm: number): void;
+  reset(): void;
+}
