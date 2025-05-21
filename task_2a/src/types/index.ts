87888ad645d5f6b9cb63871984899a819f82baaa
proyev <prots.yevhen@gmail.com>
2025-05-21 10:19:50 +0200

    init simulation types

---
+++ task_2a/src/types/index.ts
@@ -0,0 +1,21 @@
+export type SimulationParams = {
+  numChargePoints: number;
+  arrivalProbabilityMultiplier: number; // percentage
+  evConsumption: number; // kWh/100km
+  chargePower: number; // kW
+};
+
+export type DataPoint = {
+  timestamp: Date;
+  powerConsumption: number; // kW
+  numActiveChargePoints: number;
+};
+
+// TODO - make it extension of prev task type
+export type SimulationResult = {
+  totalEnergyConsumed: number; // kWh
+  actualMaxPowerDemand: number; // kW
+  theoreticalMaxPowerDemand: number; // kW
+  concurencyFactor: number; // percentage
+  dataPoints: DataPoint[];
+};
